8820df5810106a19aae9e2f7469a713def9d7bc7
marcelbuesing <mbuesing@synapticon.com>
2014-01-17 11:35:47 +0100

    Integrate changes e.g. find file in project.

--- emacs-personal/sammy.el
+++ emacs-personal/sammy.el
@@ -4,13 +4,15 @@
 
 ;;; Code:
 
-;; Ensure all the modules are available.
-(prelude-require-packages '(paredit
-                            cljsbuild-mode
-                            solarized-theme
-                            multiple-cursors
-                            auto-complete
-                            expand-region))
+;; Ensure all the modules are available, excluding structured-haskell-mode.
+(prelude-require-packages '(paredit 
+			    cljsbuild-mode 
+			    monokai-theme 
+			    multiple-cursors
+			    find-file-in-project 
+			    auto-complete 
+			    yasnippet
+			    expand-region))
 
 ;; No flyspell anymore (for now)
 (setq prelude-flyspell nil)
@@ -18,40 +20,39 @@
 ;; No flycheck for SCSS
 (eval-after-load 'flycheck
   '(setq-default flycheck-disabled-checkers '(scss)))
-
-;; Use solarized
-(load-theme 'solarized-dark t)
+;;;
+;;; Use monokai theme
+;;;
+(require 'monokai-theme)
+(load-theme 'monokai t)
 
 ;;;
-;;; Standard-deviating keybindings
+;;; Find file in project (.git)
 ;;;
-
-;; Better buffer cycling
-(global-set-key [C-tab] 'previous-buffer)
-(global-set-key [C-S-iso-lefttab] 'next-buffer)
-(global-set-key (kbd "C-S-SPC") 'set-rectangular-region-anchor)
-(global-set-key (kbd "M-s l") 'sort-lines)
-
-;;;
-;;; Structured Haskell mode
-;;;
-
-;; Load SHM properly
-(add-to-list 'load-path "/home/sammy/git/structured-haskell-mode/elisp")
-(require 'shm)
-(add-hook 'haskell-mode-hook 'structured-haskell-mode)
-
-;; Configure SHM executable location
-(setq shm-program-name
-      "/home/sammy/git/structured-haskell-mode/dist/build/structured-haskell-mode/structured-haskell-mode")
-
-;; Configure SHM colors for solarized-dark
-(set-face-background 'shm-current-face "#073642") ; solarized-base02
+(require 'find-file-in-project)
+(setq ffip-project-file ".gitmodules")
+(setq ffip-find-options "-not -regex \".*vendor.*\" -not -regex \".*generated.*\" -not -regex \".*dist.*\" -not -regex \".*node_modules.*\"")
+(setq ffip-patterns (concatenate 'list '("*.haml" "*.erb" "*.sass" "*.scss" "*.xml" "*.yml" "*.js" "*.coffee" "*.json") ffip-patterns))
+(setq ffip-limit 6096)
+(global-set-key (kbd "C-o") 'find-file-in-project)
 
 ;;;
-;;; Paredit
+;;; Switch buffer with C-tab
 ;;;
-(add-hook 'clojure-mode-hook 'paredit-mode)
+(global-set-key (kbd "<C-tab>") 'next-buffer)
+(global-set-key (kbd "<C-S-iso-lefttab>") 'previous-buffer)
+
+;;;
+;;; Store Session when closing
+;;;
+;;(desktop-save-mode 1)
+
+;;;
+;;; auto-complete
+;;;
+(require 'auto-complete-config)
+(ac-config-default)
+(add-to-list 'ac-modes 'coffee-mode)
 
 ;;;
 ;;; Ace-jump-mode
@@ -66,6 +67,16 @@
 (global-set-key (kbd "C-\"") 'mc/mark-all-like-this)
 
 ;;;
+;;; Yasnippet
+;;;
+(require 'yasnippet)
+;; (require 'angularsnippet)
+(yas-global-mode 1)
+;; git clone https://github.com/AndreaCrotti/yasnippet-snippets.git ~/.emacs.d/snippets
+(add-to-list 'yas/root-directory "~/.emacs.d/snippets")
+
+
+;;;
 ;;; Pending delete mode
 ;;;
 (pending-delete-mode 1)
@@ -77,51 +88,24 @@
 (global-set-key (kbd "C-@") 'er/expand-region)
 
 ;;;
-;;; auto-complete
+;;; Paredit
 ;;;
-(require 'auto-complete-config)
-(ac-config-default)
-
-(dolist (mode '(coffee-mode scss-mode))
-  (add-to-list 'ac-modes mode))
+(add-hook 'clojure-mode-hook 'paredit-mode)
 
 ;;;
 ;;; dirtree
 ;;;
 (require 'dirtree)
-
-(defun dirtree-for-buffer ()
-  "Open dirtree in the current buffer's directory or the default directory."
-  (interactive)
-  (let (current-file (buffer-file-name))
-    (if current-file
-        (dirtree (file-name-directory current-file) nil)
-      (dirtree default-directory nil))))
-
-;; Dirtree instead of dired
 (global-set-key (kbd "C-x d") 'dirtree)
-(global-set-key (kbd "C-x C-j") 'dirtree-for-buffer)
 
 ;;;
 ;;; coffee-mode
 ;;;
 (custom-set-variables '(coffee-tab-width 2))
 
-;;;
-;;; Inline Elisp
-;;;
-
-(defun eval-and-replace ()
-  "Replace the preceding sexp with its value."
-  (interactive)
-  (backward-kill-sexp)
-  (condition-case nil
-      (prin1 (eval (read (current-kill 0)))
-             (current-buffer))
-    (error (message "Invalid expression")
-           (insert (current-kill 0)))))
-
-(global-set-key (kbd "C-x C-e") 'eval-and-replace)
+;; Circumflex
+;;(define-key key-translation-map [dead-circumflex] "^")
 
 (provide 'sammy)
 ;;; sammy.el ends here
+;;

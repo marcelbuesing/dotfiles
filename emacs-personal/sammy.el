cd15c97a9075287d7838c2c836fcb501a1e2703b
marcelbuesing <mbuesing@synapticon.com>
2014-01-18 14:46:13 +0100

    Add auto indent(format) / cleanup-buffer.

--- emacs-personal/sammy.el
+++ emacs-personal/sammy.el
@@ -75,6 +75,26 @@
 ;; git clone https://github.com/AndreaCrotti/yasnippet-snippets.git ~/.emacs.d/snippets
 (add-to-list 'yas/root-directory "~/.emacs.d/snippets")
 
+;;;
+;;; Cleanup Buffer (Autoformat Code)
+;;;
+
+(defun untabify-buffer ()
+  (interactive)
+  (untabify (point-min) (point-max)))
+
+(defun indent-buffer ()
+  (interactive)
+  (indent-region (point-min) (point-max)))
+
+(defun cleanup-buffer ()
+  "Perform a bunch of operations on the whitespace content of a buffer.
+Including indent-buffer, which should not be called automatically on save."
+  (interactive)
+  (untabify-buffer)
+  (delete-trailing-whitespace)
+  (indent-buffer))
+(global-set-key (kbd "C-x c") 'cleanup-buffer)
 
 ;;;
 ;;; Pending delete mode

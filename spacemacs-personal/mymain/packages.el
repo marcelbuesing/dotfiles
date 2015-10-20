981f18d2e7873a97b44c53d1519973a420bd5751
marcelbuesing <buesing.marcel@googlemail.com>
2015-10-20 18:42:55 +0200

    Fix magit and multiple-cursor spacemacs setup

--- spacemacs-personal/mymain/packages.el
+++ spacemacs-personal/mymain/packages.el
@@ -2,6 +2,7 @@
   '(
     ;; package mymain go here
     multiple-cursors
+    magit
     )
   "List of all packages to install and/or initialize. Built-in packages
 which require an initialization must be listed explicitly in the list.")
@@ -11,14 +12,14 @@ which require an initialization must be listed explicitly in the list.")
 
 ;; For each package, define a function mymain/init-<package-mymain>
 ;;
-(defun mymain/init-mymain-mode()
-
-  (use-package multiple-cursors-mode)
-  
-
+(defun mymain/init-multiple-cursors()
   ;;;
   ;;; multiple-cursors
   ;;;
   (global-set-key (kbd "M-+") 'mc/mark-next-like-this)
   (global-set-key (kbd "M-#") 'mc/mark-all-like-this)
 )
+
+(defun mymain/init-magit()
+  (global-magit-file-mode)
+  )

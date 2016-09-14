e5bc22884f996ae1104c9fd81bcfed2ce5df0125
Marcel Buesing <marcel.buesing@daimler.com>
2016-09-14 08:19:02 +0200

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,11 @@
+# Clone repo
+# git@github.com:marcelbuesing/dotfiles.git ~/dotfiles
+
+# Setup Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Setup Spacemacs
+git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+
+ln -s ~/dotfiles/.spacemacs ~/.spacemacs
+ln -s ~/dotfiles/spacemacs-personal/my-main ~/.emacs.d/private/
